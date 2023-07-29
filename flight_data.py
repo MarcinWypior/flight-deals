3927e12e582799078b0dfd0c9dcafea000ff1ff5
MarcinWypior <marcinwypior1990@gmail.com>
2023-07-29 17:28:37 +0200

    api still doesnt work

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,21 @@
+import requests
+import os
 class FlightData:
-    #This class will be responsible for structuring the flight data.
-    pass
+    def __init__(self):
+        self.url="http://api.aviationstack.com/v1/flights"
+        self.aviationstack_key='d507a8851c00cece9330822322bb4c8e'
+        self.iata_code_london="LHR"
+
+    def get_info(self):
+        params = {
+            'access_key': self.aviationstack_key,
+            'dep_iata':"LHR",
+            'arr_iata':"CDG",
+        }
+        response = requests.get(url=self.url, params=params)
+        return response
+
+if __name__ == "__main__":
+    flightdata = FlightData()
+    print(flightdata.aviationstack_key)
+    print(flightdata.get_info().json())
--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,20 @@
+import requests
+
+
 class FlightSearch:
-    #This will be responsible for talking to the Flight Search API.
-    pass
+    def get_categories(self):
+        import requests
+        url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/monthly"
+        querystring = {"destination": " MOW", "origin": "LED", "length": "3", "currency": "RUB"}
+        headers = {
+            "X-Access-Token": "<REQUIRED>",
+            "X-RapidAPI-Key": "82d0e9f98fmsh75408924d4ce198p136f97jsn17ebda80ba66",
+            "X-RapidAPI-Host": "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com"
+        }
+
+        response = requests.get(url, headers=headers, params=querystring)
+
+        print(response.json())
+
+flight_serach = FlightSearch()
+flight_serach.get_categories()
